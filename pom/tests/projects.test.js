d87d933e9ae1c65690cb946549f6cf65e90dd2ea
Elizabeth Cobián <elizabeth.cobian@wizeline.com>
2021-07-29 18:24:31 -0500

    Creating the new repo for the Todoist-API bootcamp
    - I'm creating the new repo with the project for the Todoist-API
    task

---
+++ pom/tests/projects.test.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+import { URLS, PROJECT } from '../data/Constants'
+import appToday from '../pages/appToday'
+import projects from '../pages/projectsPage'
+import { STANDAR_USER } from '../data/Roles'
+
+fixture('Projects')
+  .page(URLS.LOGIN_URL)
+  .beforeEach(async t => {
+    await t.useRole(STANDAR_USER)
+    await t.expect((appToday.todayTitle).exists).ok({ timeout: 10000 })
+  })
+
+test('Creating a new project', async t => {
+  await projects.addProject(PROJECT.PROJECT_NAME)
+  await t.expect((projects.projectAdded).exists).ok()
+})
